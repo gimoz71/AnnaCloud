9b7b71c367c661c9e1216ffea88ae49c7d6d6842
LaganaDavide <lagana.davide89@gmail.com>
2018-10-29 14:05:44 +0100

    allineamento

--- javascripts/app/loginApp/controller/preferitiController.js
+++ javascripts/app/loginApp/controller/preferitiController.js
@@ -1,5 +1,5 @@
-angular.module("loginModule").controller("preferitiController", ["$scope", "getConfigurazioniService", "loginService",
-	function($scope, getConfigurazioniService, loginService) {
+angular.module("loginModule").controller("preferitiController", ["$scope", "getConfigurazioniService", "loginService", "listeService",
+	function($scope, getConfigurazioniService, loginService, listeService) {
 
 	$scope.listaPreferiti = [];
 	loginService.getUserAttributes().then(
@@ -22,6 +22,43 @@ angular.module("loginModule").controller("preferitiController", ["$scope", "getC
 			console.log(reason)
 		}
 	)	
+	
+	$scope.addCarrello = function (conf){
+		loginService.getUserAttributes().then(
+			function (attList){
+				console.log(attList);
+				attList.forEach(function (a){
+					if (a["Name"] == "email" ){
+						codice = a["Value"];
+						console.log(codice);
+						getConfigurazioniService.response(codice).then(function(data){
+							let ord  = {
+							  "codiceOrdine" : "",
+							  "configurazione": conf,
+							  "pagato": 0,
+							  "stato": 0,
+							  "utente": {
+							    "email": codice
+							  }
+							}
+							listeService.putOrdine(ord).then(
+									function (res){
+										console.log(res)
+									},
+									function (reason){
+										console.log(reason)
+									}
+								);
+						})
+					}
+				})
+			},
+			function (reason){
+				console.log(reason)
+			}
+		)	
+		
+	}
 //		getConfigurazioniService.response("john@bea.com").then(function (data) {
 //			$scope.listaPreferiti = data.data.configurazioni;
 //			console.log(data);
