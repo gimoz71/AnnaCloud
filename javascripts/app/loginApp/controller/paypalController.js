1c03f5afd3507d748e7c71b1e277825956fdd169
Paolo Salvadori <oloap1981@gmail.com>
2018-12-06 09:59:20 +0100

    aggiunta logica a pagina ordini

--- javascripts/app/loginApp/controller/paypalController.js
+++ javascripts/app/loginApp/controller/paypalController.js
@@ -2,79 +2,6 @@
 		function($scope, loginService, listeService) {
 			
 		 paypal.Button.render(
-			//  {
-			//     // Configure environment
-			//     env: 'sandbox',
-			//     client: {
-			//       sandbox: 'AT1iiIpFRZhumzfgjO0TKil1W1MsCHFXzqhcbB8o7cCqfdujMn0o1ie1b1pwHgzqBavu7pp0WQoeq4X4',
-			//       //production: 'demo_production_client_id'
-			//     },
-			//     // Customize button (optional)
-			//     locale: 'it_IT',
-			//     style: {
-			//       size: 'responsive',
-			//       color: 'blue',
-			//       shape: 'rect'
-			//     },
-			//     // Set up a payment
-			//     payment: function(data, actions) {
-			// 			return new paypal.Promise(function(resolve, reject) {
-			// 				let params = {
-			// 						'action': 'create',
-			// 						'use_billing_agreement': that.recurring
-			// 				};
-			// 				MarketAPI.executeOrder(that.order.id, that.constants.PAYMENT_SYSTEMS.PAYPAL, params, true,
-			// 				function(data) {
-			// 						if (data.processed) {
-			// 								that.successfullyBuy();
-			// 								return;
-			// 						}
-			// 						that.paymentId = data.params.paymentId;
-			// 						resolve(that.paymentId);
-
-			// 				});
-			// 		});
-			//       // return actions.payment.create({
-			//       //   transactions: [{
-			//       //     amount: {
-			//       //       total: $scope.calcolaPrezzoOrdine($scope.getOrdineInCorso()) + $scope.getCostoSpedizione(),
-			//       //       currency: 'EUR'
-			//       //     }
-			//       //   }]
-			//       // });
-			//     },
-			//     // Execute the payment
-			//     onAuthorize: function(data, actions) {
-			//       return actions.payment.execute().then(
-			//     		  function(data) {
-			// 		        // Show a confirmation message to the buyer
-			// 		        window.alert("grazie per l'acquisto !");
-			// 		        console.log(data);
-					        	
-			// 						$scope.getOrdineInCorso().pagato = 1,
-			// 						$scope.getOrdineInCorso().stato = 1,
-									
-			// 						listeService.putOrdine($scope.getOrdineInCorso()).then(
-			// 							function(data){
-			// 								if(data.errorMessage != null && data.errorMessage != ""){
-			// 									console.log("ordine aggiornato");
-			// 								} else {
-			// 									console.log("problema nell'aggiornamento dell'ordine");
-			// 								}
-			// 							},
-			// 							function(err){
-			// 								console.log(err);
-			// 							}
-			// 						);
-			// 		      },
-			// 		      function(err) {
-			// 		        // Show a confirmation message to the buyer
-			// 		        window.alert("errore durante il pagamento");
-			// 		        console.log(err);
-			// 		      }
-			//       );
-			//     }
-			// 	}
 				{
 					env: 'sandbox', // sandbox | production
 
@@ -133,6 +60,10 @@
 								$scope.svuotaCarrello($scope.getOrdineInCorso());//il carrello lo svuoto solo se il pagamento è andato a buon fine
 								$scope.completaOperazioniOrdneAcquistato();
 							});
+					}, 
+
+					onError: function(err){
+						alert("Ci sono stati dei problemi durante il checkout. Il pagamento non è andato a buon fine, controllare Paypal per ulteriori dettagli");
 					}
 				}
 				, '#paypal-button' ); 
